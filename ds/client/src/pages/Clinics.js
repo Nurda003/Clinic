d70a1c7c8103f6121fa526d05e43eeab6416f43e
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-07 17:29:05 +0500

    asdasd

--- ds/client/src/pages/Clinics.js
+++ ds/client/src/pages/Clinics.js
@@ -114,7 +114,7 @@ function Clinics() {
     useEffect(() => {
         const fetchClinics = async () => {
             try {
-                const response = await axios.get(`https://dental-c-73083695c8b5.herokuapp.com/api/clinics`);
+                const response = await axios.get(`https://dental-w-032fe80aafac.herokuapp.com/api/clinics`);
                 setClinics(response.data)
                 
                 
