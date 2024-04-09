260d0ab3455adbe44d306930a42adf1c622fcd0c
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-10 01:01:38 +0500

    postbuild?

--- ds/client/src/pages/Clinics.js
+++ ds/client/src/pages/Clinics.js
@@ -189,7 +189,7 @@ function Clinics() {
                 <div key={clinic._id} className="flex w-full gap-6 p-3 rounded-2xl bg-white items-center mt-10">
                     <div className="w-heroimg h-heroimg">
                     { console.log(clinic.image.filename)
-                        (clinic.image &&
+                        (clinic.image && clinic.image.filename &&
                         <img src={`/api/image/${clinic.image.filename}`} alt={clinic.name} />
                     )}
                     </div>
