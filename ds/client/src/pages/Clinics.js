e965919837984daefb652d18f13b0d179e5f6e26
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-10 02:43:25 +0500

    I fuess finished

--- ds/client/src/pages/Clinics.js
+++ ds/client/src/pages/Clinics.js
@@ -124,6 +124,7 @@ function Clinics() {
           .catch((error) => {
             console.log(error);
           });
+          
       };
 
    
@@ -189,9 +190,6 @@ const handleSort = (event) => {
                                 <option value="priceLow">Price - Low to High</option>
                                 <option value="priceHigh">Price - High to Low</option>
                             </select>
-                            <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                            </svg>
                         </div>
                         
                     </div>
@@ -315,9 +313,6 @@ const handleSort = (event) => {
                         <label id="phoneNumber" htmlFor="phoneNumber" className='text-lg text-bigtext ml-1 mt-2 font-medium'>Phone number:</label>
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
-                                    <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
-                                </svg>
                             </div>
                             <input 
                             type="text" 
